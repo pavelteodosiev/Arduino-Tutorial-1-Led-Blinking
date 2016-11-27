5da1fe276aad3b71ab710747ae57c10528718509
Pavel Teodosiev <pavelteodosiev@gmail.com>
2016-11-27 13:23:16 +0200

    Add files via upload

---
+++ Led_blink_tutorial.ino
@@ -0,0 +1,13 @@
+int led=12;
+
+void setup(){
+  pinMode(led, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(led, HIGH); //turns the led on
+  delay(1000); //wait 1 second
+  digitalWrite(led, LOW); //turns the led off
+  delay(1000); // wait 1 second
+}
+
