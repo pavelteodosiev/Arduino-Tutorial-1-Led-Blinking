2668c12ad9fa25f1cbae2f74757b6aa949fcf188
Pavel Teodosiev <pavelteodosiev@gmail.com>
2016-12-04 23:20:28 +0200

    Add files via upload

---
+++ led.ino
@@ -0,0 +1,14 @@
+int led = 12;
+
+void setup(){
+  pinMode(led, OUTPUT);
+ 
+}
+
+void loop(){
+  digitalWrite(led, HIGH); //turn the led on
+  delay(1000); //wait 1 second
+  digitalWrite(led, LOW); //turn the led off
+  delay(1000); //wait 1 second
+}
+
